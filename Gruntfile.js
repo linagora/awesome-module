ef3dec18f063b340e07b26e4e456014f9b6fecbd
Michael Bailly <mbailly@linagora.com>
2014-10-20 15:36:20 +0200

    Add index.js in the list of linted files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'lib/**/*.js'],
+      files: ['Gruntfile.js', 'lib/**/*.js', 'index.js'],
       options: {
         jshintrc: '.jshintrc'
       }
@@ -22,6 +22,7 @@ module.exports = function(grunt) {
       src: '<%= jshint.files %>'
     }
   },
+  // fixjsstyle --disable 0110 -r lib -e ./node_modules
   fixjstyle: {
     options: {
       flags: [
